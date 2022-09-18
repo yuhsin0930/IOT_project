297cb001256eb32ffdb61706c04d3e076a2a56c5
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-09-18 14:58:01 +0800

    1. add layout in MainActivity 2. change the label and icon of App Logo.

--- app/src/main/java/com/example/iot_project/MainActivity.java
+++ app/src/main/java/com/example/iot_project/MainActivity.java
@@ -1,35 +1,51 @@
 package com.example.iot_project;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewBecomeSaller, textViewLogin;
+    private Button buttonLogin,buttonEnroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewBecomeSaller = (TextView)findViewById(R.id.textView_BecomeSaller);
-        textViewLogin = (TextView)findViewById(R.id.textView_login);
-        textViewBecomeSaller.setOnClickListener(new View.OnClickListener() {
+
+//      To hide Action Bar
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+
+//      Login button => LoginActivity
+        buttonLogin = (Button) findViewById(R.id.button_main_login);
+//      (temp) Enroll button => BecomeSellerActivity.class
+        buttonEnroll = (Button) findViewById(R.id.button_main_enroll);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonEnroll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, BecomeSellerActivity.class);
                 startActivity(intent);
             }
         });
-        textViewLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+
     }
 }
