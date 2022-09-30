bb22ed7e3c473cb2025fc7c6acec62f6f7d71cd0
yuhsin0930 <yuhsin0930@gmail.com>
2022-09-30 13:32:39 +0800

    更新許願池詳細資料

--- app/src/main/java/com/example/iot_project/MyStore/MyStoreActivity.java
+++ app/src/main/java/com/example/iot_project/MyStore/MyStoreActivity.java
@@ -107,7 +107,7 @@ public class MyStoreActivity extends AppCompatActivity {
 
 // -----------------------------------------------------------------------------------
 //  以下是予馨的願望
-//  存取現在登入賣家(member_id)的 商場照片(storePicture) 、 商場名稱(storeName) 、賣家帳號(account_name)
+//  存取現在登入賣家(member_id)的 會員照片(picture) 、 姓名(name) 、賣家帳號(account_name)
 //  存取現在登入賣家(member_id) 訂單狀態(orderStatus) =  "待出貨" 商品數量
 //  存取現在登入賣家(member_id) 訂單狀態(orderStatus) =  "不成立" 商品數量
 //  存取現在登入賣家(member_id) 訂單狀態(orderStatus) =  "退貨"&"退款" 商品數量
