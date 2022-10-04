c736bd7b1836b4b58547059af3f353113b586cc1
yuhsin0930 <yuhsin0930@gmail.com>
2022-10-04 08:39:46 +0800

    款項頁面假資料

--- app/src/main/java/com/example/iot_project/salesRecord/PaymentActivity.java
+++ app/src/main/java/com/example/iot_project/salesRecord/PaymentActivity.java
@@ -2,20 +2,54 @@ package com.example.iot_project.salesRecord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.example.iot_project.MyStore.MyStoreActivity;
 import com.example.iot_project.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PaymentActivity extends AppCompatActivity {
+
+    private ListView listView_payment;
+    private ImageView imageView_payment_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         setWindow();
+        imageView_payment_back = (ImageView)findViewById(R.id.imageView_payment_back);
+        listView_payment = (ListView) findViewById(R.id.listView_payment);
+        List<Map<String,Object>> paymentList= new ArrayList<>();
+        Map<String,Object> paymentMap = new HashMap<>();
+        for(int i=0;i<30;i++){
+            paymentMap.put("orderNum","F123456789");
+            paymentMap.put("totalPrice","800");
+            paymentMap.put("createTime","2022-9-28");
+            paymentList.add(paymentMap);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(PaymentActivity.this,paymentList,R.layout.payment_listview_items,new String[]{ "orderNum","totalPrice","createTime"},new int[]{
+                R.id.textView_payment_orderNumber,R.id.textView_payment_totalAmount,R.id.textView_payment_orderDate});
+        listView_payment.setAdapter(simpleAdapter);
+
+        imageView_payment_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PaymentActivity.this, MyStoreActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
     private void setWindow() {
